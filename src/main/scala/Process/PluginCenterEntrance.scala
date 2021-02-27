3bf754b914115e8f8f6994b6eb88468efa63a45d
yifan <yifzhang@pku.edu.cn>
2021-02-28 00:21:44 +0800

    Update

--- src/main/scala/Process/PluginCenterEntrance.scala
+++ src/main/scala/Process/PluginCenterEntrance.scala
@@ -1,7 +1,13 @@
 package Process
 
+import Globals.GlobalVariables
+import Plugins.EngineOperationAPI.TreeObjectSyncClient
+import Plugins.MSUtils.AkkaBase.AkkaUtils
 import Plugins.MSUtils.AkkaBase.AkkaUtils.clusterSystem
 import Plugins.MSUtils.MailSender
+import Plugins.MSUtils.AkkaBase.AkkaUtils
+import Plugins.MSUtils.{API, MailSender, ServiceUtils}
+import akka.actor.typed.ActorSystem
 
 /** 程序入口 */
 object PluginCenterEntrance {
@@ -9,6 +15,11 @@ object PluginCenterEntrance {
     println("=== master version (display) ===")
     println("Hello world!")
     println("setting up", clusterSystem)
+
+    Thread.sleep(10000)
+    GlobalVariables.akkaSyncClient = ActorSystem(TreeObjectSyncClient(), "syncClient")
+    DisplayHttpServer.startHttpServer(new DisplayRoutes()(AkkaUtils.clusterSystem).displayRoutes, AkkaUtils.clusterSystem)
+
   } catch {
     case exception: Exception =>
       MailSender.emailException(exception)
